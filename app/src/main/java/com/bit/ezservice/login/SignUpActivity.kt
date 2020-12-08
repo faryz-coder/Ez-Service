eea42de3a4d2ea3459ae0f3f7b291556fce679fc
faryz-coder <faryz.coder@gmail.com>
2020-12-09 05:28:18 +0800

    ez service app

---
+++ app/src/main/java/com/bit/ezservice/login/SignUpActivity.kt
@@ -0,0 +1,12 @@
+package com.bit.ezservice.login
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.bit.ezservice.R
+
+class SignUpActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.login_signup_activity)
+    }
+}
